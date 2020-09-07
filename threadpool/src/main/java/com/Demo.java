def70aad4d8006dd2e06d5e44b0f3c0b31f1a63d
YueGod <31380473@qq.com>
2020-09-08 00:39:21 +0800

    手写实现一个简单的线程池

--- threadpool/src/main/java/com/Demo.java
+++ threadpool/src/main/java/com/Demo.java
@@ -13,7 +13,7 @@ public class Demo {
     public static SimpleThreadPoolExecutor executor = new SimpleThreadPoolExecutor(2, 4);
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 10; i++) {
             threadPool();
 //            Thread.sleep(500);
         }
